ae0cc44ba62389f0597042546e12f001731fbb4a
Lucas <lucaslcslcs1998@gmail.com>
2022-06-28 14:38:56 -0300

    Jetpark View Binding Kotlin

--- app/src/main/java/me/dio/matches_simulador_app/MainActivity.kt
+++ app/src/main/java/me/dio/matches_simulador_app/MainActivity.kt
@@ -2,10 +2,19 @@ package me.dio.matches_simulador_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import me.dio.matches_simulador_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //setContentView(R.layout.activity_main)
+        //val tvHello : TextView = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.tvHello.text = "Teste"
     }
 }

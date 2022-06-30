7a24f0b54cf6008c549ce0e15a7689c912b472a4
Lucas <lucaslcslcs1998@gmail.com>
2022-06-30 14:48:01 -0300

    Criação de Layouts do App: Criandp a Activity de Detalhe em Kotlin

---
+++ app/src/main/java/me/dio/matches_simulador_app/DetailActivity.kt
@@ -0,0 +1,17 @@
+package me.dio.matches_simulador_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.matches_simulador_app.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
